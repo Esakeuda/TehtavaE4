86b2372800884a97cce1fdd4eab750569f1eda8f
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 08:49:47 +0300

    Add project files.

---
+++ TehtavaE4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumeroKysely
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Syötä ensimmäinen kokonaisluku: ");
+                int luku1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Syötä toinen kokonaisluku: ");
+                int luku2 = int.Parse(Console.ReadLine());
+
+                if (luku1 > 0 && luku2 > 0)
+                {
+                    Console.WriteLine("Molemmat ovat positiivisia.");
+                }
+                else if (luku1 < 0 && luku2 < 0)
+                {
+                    Console.WriteLine("Molemmat ovat negatiivisia.");
+                }
+                else
+                {
+                    Console.WriteLine("Toinen on positiivinen ja toinen negatiivinen.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Virheellinen syöte. Syötä kokonaislukuja.");
+            }
+        }
+    }
+}
+
